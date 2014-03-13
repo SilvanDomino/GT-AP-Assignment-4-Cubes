aeffa71d003f7d08009e289923cfce169b79f7f0
Silvan Herrema <sil489@hotmail.com>
2014-03-13 11:46:32 +0100

    Rigidbody working

--- src/springs.cpp
+++ src/springs.cpp
@@ -11,29 +11,31 @@
 using namespace cyclone;
 class Springs : public RigidBodyApplication
 {
+	/** Processes the contact generation code. */
+    virtual void generateContacts();
 
+    /** Processes the objects in the simulation forward in time. */
+    virtual void updateObjects(cyclone::real duration);
+
+    /** Resets the position of all the bones. */
+    virtual void reset();
 public:
     /** Creates a new demo object. */
     Springs();
     virtual ~Springs();
-
     /** Returns the window title for the demo. */
     virtual const char* getTitle();
-
     /** Display the particles. */
     virtual void display();
-
     /** Update the particle positions. */
     virtual void update();
-
 	 /** Handle a mouse click. */
     virtual void mouse(int button, int state, int x, int y);
-
     /** Handle a mouse drag */
     virtual void mouseDrag(int x, int y);
-
     /** Handles a key press. */
     virtual void key(unsigned char key);
+	
 
 	Cube cube1;
 	LevelGraph lvlGraph;
@@ -49,6 +51,7 @@ Springs::~Springs()
 {
 
 }
+
 void Springs::key(unsigned char key){
 	RigidBodyApplication::key(key);
 }
@@ -63,13 +66,20 @@ void Springs::display()
 	RigidBodyApplication::display();
 	cube1.displayCube();
 	lvlGraph.displayLevel();
+}
+void Springs::reset(){
 
-    
 }
 
 void Springs::update()
 {
     RigidBodyApplication::update();
+}
+void Springs::generateContacts(){
+
+}
+void Springs::updateObjects(real duration){
+
 }
 
 const char* Springs::getTitle()
